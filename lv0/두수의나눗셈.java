193a51b971824f18f1c324ee28749187251ef974
chaeyoon <chaeyoon010809@gmail.com>
2023-08-21 10:55:43 +0900

    사칙연산

---
+++ lv0/두수의나눗셈.java
@@ -0,0 +1,10 @@
+package lv0;
+
+public class 두수의나눗셈 {
+
+	public int solution(int num1, int num2) {
+        int answer = (num1 * 1000) / num2;
+        return answer;
+    }
+	
+}
---
+++ lv0/숫자비교하기.java
@@ -0,0 +1,12 @@
+package lv0;
+
+public class 숫자비교하기 {
+
+public int solution(int num1, int num2) {
+        
+        if (num1 == num2) return 1;
+        return -1;
+        
+    }
+	
+}
